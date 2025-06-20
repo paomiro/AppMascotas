68104bc099f66649ae393e2e419e152bda0d0061
Paola <paomiro@gmail.com>
2025-06-20 09:02:27 -0600

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Paola Miranda on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
